3addb2197c55049072a5476d4d9b296a777f0d06
johnjaiderjaramillo <47198446+johnjaiderjaramillo@users.noreply.github.com>
2019-03-01 20:39:27 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+hola a todos esto es la prueba de readme.md, gracias# ejemplo1platzi
+
+primera prueba platzi
+# prueba1-platzi
+# version-dos-de-prueba-en-repositorios
---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("hola, esta sería la actualización .......3");
+console.log("hola, esta sería la actualización .......2");
+console.log("hola, esta sería la actualización .......1");
+console.log("hola, esta sería la actualización .......0");
